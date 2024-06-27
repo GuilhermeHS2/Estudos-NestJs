0bf30fd2722f8943dd266b013ed175f329c94c95
GuilhermeHS2 <guilhermehsantos00@gmail.com>
2024-06-26 21:54:34 -0300

    Adicionando o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { userModule } from './user/user.module';
+import { prismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [userModule],
+  imports: [userModule, prismaModule],
   controllers: [AppController],
   providers: [AppService],
   exports: [AppService]
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { userController } from "./user.controller";
+import { UserService } from "./user.service";
+import { prismaModule } from "src/prisma/prisma.module";
 
 @Module({
-    imports: [],
+    imports: [prismaModule],
     controllers: [userController],
-    providers: [],
+    providers: [UserService],
     exports: []
 })
 export class userModule {}
